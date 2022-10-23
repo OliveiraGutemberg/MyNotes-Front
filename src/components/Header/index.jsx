d0c4ac8bf449a66647728451adf81b25b941bdf5
Gugu <gutembergtrabalho@gmail.com>
2022-10-22 21:33:06 -0300

    change in avatar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import { useAuth } from "../../hooks/auth"
 import { Container, Profile, Logout } from "./styles";
 import { api } from "../../services/api";
 
+import avatarPlaceholder from "../../assets/blank-profile-picture-973460.svg"
+
 export function Header(){
     const { signOut, user } = useAuth();
 
